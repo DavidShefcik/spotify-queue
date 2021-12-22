f78561a9481c5884aa562a369fd4ad9fcffbc543
DavidShefcik <davidshefcik@gmail.com>
2021-12-22 00:01:58 -0600

    Replace ASP.NET with Python. Application Docker compose config

---
+++ api/src/app.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from dotenv import load_dotenv
+
+from fastapi import FastAPI
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
